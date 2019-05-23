50a6133844e2cb6d50772ebb9f72da45723c7159
Priyanka <sahu.priyanka@hotmail.com>
2019-05-23 13:00:07 +0530

    Update dname.php

--- dname.php
+++ dname.php
@@ -102,10 +102,6 @@ echo( img src= 'img/me-Copy.jpg'  ALIGN='right' style='top:-16 px'/>");
           <p class='text-size-12'>Copyright </p>
           <p class='text-size-12'></p>
         </div>
-        <div class='s-12 l-6'>
-
-        <a class='right text-size-12' href='http://www.myresponsee.com' title='Responsee - lightweight responsive framework'>Design and coding<br> by Responsee Team</a>
-        </div>
       </section>
      <script type='text/javascript' src='js/responsee.js'></script>
     <script type='text/javascript' src='owl-carousel/owl.carousel.js'></script>
