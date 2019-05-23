fa586e2639db23060d837e249095e2aff606545a
SahuPriyanka <50921869+SahuPriyanka@users.noreply.github.com>
2019-05-23 12:44:08 +0530

    Update dname.php

--- dname.php
+++ dname.php
@@ -89,7 +89,7 @@ $con = new mysqli("localhost","root","","aadhar");
 		echo("<h2>Aadhar not registered</h2>");
 
 
-echo("<img src='img/vasu1.jpg'  ALIGN='right' style='top:-16 px'/>");
+echo( src='img/me-Copy.jpg'  ALIGN='right' style='top:-16 px'/>");
 }session_destroy();
 ?>
 </p></div>
