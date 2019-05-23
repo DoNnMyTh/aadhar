6c7fc5226365aa9d0c4b306e7508e07a3e7a9424
Priyanka <sahu.priyanka@hotmail.com>
2019-05-23 13:39:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -114,9 +114,6 @@ echo("Invalid login!!");
           <p class="text-size-12">Copyright </p>
           <p class="text-size-12"></p>
         </div>
-        <div class="s-12 l-6">
-          <a class="right text-size-12" href="http://www.myresponsee.com" title="Responsee - lightweight responsive framework">Design and coding<br> by Responsee Team</a>
-        </div>
       </section>
     </footer>
     <script type="text/javascript" src="js/responsee.js"></script>
