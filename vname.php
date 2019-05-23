97bfa21eb6e3714ada3b3ac0a4acce5d67a88b43
SahuPriyanka <50921869+SahuPriyanka@users.noreply.github.com>
2019-05-23 12:54:23 +0530

    Update vname.php

--- vname.php
+++ vname.php
@@ -94,10 +94,7 @@ $con = new mysqli("localhost","root","","aadhar");
           <p class='text-size-12'>Copyright </p>
           <p class='text-size-12'></p>
         </div>
-        <div class='s-12 l-6'>
-
-        <a class='right text-size-12' href='http://www.myresponsee.com' title='Responsee - lightweight responsive framework'>Design and coding<br> by Responsee Team</a>
-        </div>
+       
       </section>
      <script type='text/javascript' src='js/responsee.js'></script>
     <script type='text/javascript' src='owl-carousel/owl.carousel.js'></script>
