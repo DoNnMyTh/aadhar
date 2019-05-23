a9978039ec5397426baa02aa447d61c72379a618
amit chaudhary <amit2cha@hotmail.com>
2019-05-23 09:36:02 +0530

    fixed session

--- dlSession.php
+++ dlSession.php
@@ -1,21 +1,46 @@
 <?php
-$username = $_POST["username"];
-$username = trim($username);
-$password = $_POST["password"];
-$password = trim($password);
-$u = "dladmin";
-$p = "car123";
-if($username == $u && $password == $p) 
+
+/*
+all settings present in dbCon.php;
+$servername = 'localhost';
+$username = "root";
+$password = "";
+$dbname = "admin";
+table name = adminRole;
+table must have columns username , password , role;
+connection variable $conn;
+and make username as primary key
+
+
+*/
+
+//post data and included files
+include 'dbCon.php';
+$usernamePost = $_POST["username"];
+$passwordPost = $_POST["password"];
+
+//sql query to fetch details
+$sql = "SELECT * FROM adminRole WHERE username='"."$username"."';";
+$result = mysqli_query($conn, $sql);
+
+//passing the query and it will return a object
+$value = mysql_fetch_object($result);
+
+$usernameSql = $value->username;
+$passwordSql = $value->password;
+$roleSql = $value->role;
+
+
+if($usernamePost == $usernameSql && $passwordSql == $p) 
 {
 	print "Access granted";
 	session_start();
-	$_SESSION["Basic details, dl"] = 'true';
-	header('Location:menu.php');
+	$_SESSION["role"] = $roleSql;
+	//header('Location:menu.php');
 }
 else 
 {
-	print $username;
-	print $password;
-	header('Location: Data.html');
+	echo "failed";
+	//header('Location: Data.html');
 }
 ?>
