f302faced6797c8748d4353648544a28a5b3c0e2
SahuPriyanka <50921869+SahuPriyanka@users.noreply.github.com>
2019-05-23 12:57:49 +0530

    Update mnane.php

--- mnane.php
+++ mnane.php
@@ -91,10 +91,7 @@ session_destroy();
           <p class='text-size-12'>Copyright </p>
           <p class='text-size-12'></p>
         </div>
-        <div class='s-12 l-6'>
-
-        <a class='right text-size-12' href='http://www.myresponsee.com' title='Responsee - lightweight responsive framework'>Design and coding<br> by Responsee Team</a>
-        </div>
+ 
       </section>
      <script type='text/javascript' src='js/responsee.js'></script>
     <script type='text/javascript' src='owl-carousel/owl.carousel.js'></script>
