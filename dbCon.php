7ef5a832df77d6123695d009db8dc3097ddc57a8
amit chaudhary <amit2cha@hotmail.com>
2019-05-25 16:28:48 +0530

    dbrole

--- dbCon.php
+++ dbCon.php
@@ -2,7 +2,7 @@
 $servername = 'localhost';
 $username = "root";
 $password = "";
-$dbname = "admin";
+$dbname = "namitadb";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
