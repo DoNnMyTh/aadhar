d810d4756ad972b791ad7f07ddf72a0074ba65b0
Priyanka <sahu.priyanka@hotmail.com>
2019-05-28 11:53:34 +0530

    Update RegisterForm.php

--- RegisterForm.php
+++ RegisterForm.php
@@ -12,7 +12,7 @@
           $con = mysqli_connect("localhost","root","","namitadb");
      
           $qry = 'INSERT INTO register VALUES ("'.$username.'","'.$password.'","'.$gender.'","'.$dob.'","'.$phone.'","'.$state.'","'.$city.'","'.$email.'");';
-              echo ($qry."<br>");
+              
               if (mysqli_query($con, $qry)) {
                 echo "New record created successfully";
             } else {
